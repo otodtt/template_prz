126fb7a8caac6b55bd325c3a5e384a58cd7f4495
otodtt <dtenevoto@mail.bg>
2018-03-14 01:04:10 +0200

    13.03.2018 -2

--- resources/views/crops/index.blade.php
+++ resources/views/crops/index.blade.php
@@ -291,7 +291,7 @@
             </table>
         </div>
         <div class="row">
-            <h4>Украсни</h4>
+            <h4>Украсни и Горски видове</h4>
             <table class="table">
                 @foreach($cultures as $culture)
                     @if($culture->group_id == 13)
@@ -315,34 +315,10 @@
             </table>
         </div>
         <div class="row">
-            <h4>Горски видове</h4>
-            <table class="table">
-                @foreach($cultures as $culture)
-                    @if($culture->group_id == 14)
-                        <tr>
-                            <td>{{$culture->id}}</td>
-                            <td>{{$culture ->name}}</td>
-                            <td>{{$culture ->latin_name}}</td>
-                            <td>
-                                <a href="{!!URL::to('/crops/edit/'.$culture->id)!!}" class="fa fa-edit btn btn-primary">
-                                    &nbsp;Редактирай!
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{!!URL::to('/crops/show/'.$culture->id)!!}" class="fa fa-edit btn btn-success">
-                                    &nbsp;ВИЖ КУЛТУРАТА!
-                                </a>
-                            </td>
-                        </tr>
-                    @endif
-                @endforeach
-            </table>
-        </div>
-        <div class="row">
             <h4>Цитросови</h4>
             <table class="table">
                 @foreach($cultures as $culture)
-                    @if($culture->group_id == 15)
+                    @if($culture->group_id == 14)
                         <tr>
                             <td>{{$culture->id}}</td>
                             <td>{{$culture ->name}}</td>
